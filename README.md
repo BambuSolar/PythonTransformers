ebd307dbd2b2159a8f6da36adf403366203f9b8e
Javier Caballero <caballerojavier13@gmail.com>
2017-02-24 23:04:49 -0300

    Resolución de problemas con gitignore.

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 Transformer Deployer
 ========
 
+##Instalación
+
+1. Clonar el repositorio
+ 
+`git clone git@github.com:BambuSolar/transformer_deployer.git`
+
+2. Instalar dependencias
+
+    - `sudo apt-get install python3`
+    - `sudo apt-get install python3-pip`
+    - `sudo apt-get install virtualenv python-virtualenv`
+
+3. Ejecutar el siguiente comando
+
+`export LC_ALL=C`
+
+4. Cambiar al directorio donde se instaló el Transformer Deployer
+
+`cd transformer_deployer`
+
+5. Instalar en virtual environment
+
+`virtualenv venv`
+
+6. Activar en virtaul environment
+
+`source venv/bin/activate`
+
+7. Instalar las dependencias
+
+`pip install -r requirements.txt`
+
+8. Ejecutar el servidor
+
+`python transformers_server.py`
---
+++ app/api/build/build_controller.py
@@ -0,0 +1,88 @@
+from flask import Blueprint, jsonify, request
+from flask_api import status, exceptions
+from crawler.main import HTMLCrawler
+from git import local_git
+
+bp = Blueprint('builds', __name__)
+buildsBluePrint = Blueprint('builds', __name__)
+
+
+@buildsBluePrint.route('/api/builds', methods=['POST'])
+def create():
+
+    body = request.json
+
+    if body:
+
+        try:
+
+            url = body['url']
+
+            environment = body['environment']
+
+            if environment not in ['prod', 'staging', 'beta']:
+
+                raise exceptions.ParseError(
+                    "Los environments aceptados son %s" % (str(['prod', 'staging', 'beta']))
+                )
+
+            before_deploy = local_git.get_last_version(environment)
+
+            crawler = HTMLCrawler(url)
+
+            crawler.run(environment)
+
+            after_deploy = local_git.get_current_version()
+
+            if before_deploy == after_deploy:
+                content = jsonify({
+                    "data": after_deploy
+                })
+
+                return content, status.HTTP_200_OK
+
+            else:
+
+                content = jsonify({
+                    "data": after_deploy
+                })
+
+                return content, status.HTTP_201_CREATED
+
+        except KeyError as e:
+
+            raise exceptions.ParseError(
+                "%s es un dato requerido" % (str(e)[1:-1].capitalize())
+            )
+
+    else:
+
+        raise exceptions.ParseError(detail=None)
+
+
+@buildsBluePrint.route('/api/builds', methods=['GET'])
+def list_all():
+
+    content = jsonify(local_git.get_all_versions('all'))
+
+    return content, status.HTTP_200_OK
+
+
+@buildsBluePrint.route('/api/builds/current', methods=['GET'])
+def current():
+
+    content = jsonify({
+        "data": local_git.get_current_version()
+    })
+
+    return content, status.HTTP_200_OK
+
+
+@buildsBluePrint.route('/api/builds/last', methods=['GET'])
+def last():
+
+    content = jsonify({
+        "data": local_git.get_last_version('prod')
+    })
+
+    return content, status.HTTP_200_OK
